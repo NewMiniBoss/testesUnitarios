fe3beade04840227e7bff28cebab706a59529253
gabriel <gabriel.f.lazari@gmail.com>
2023-10-04 22:43:41 -0300

    implementnado outro nivel de test

--- test/pagament.test.js
+++ test/pagament.test.js
@@ -5,3 +5,9 @@ test('retorno das horas extras', ()=> {
     const retorno = somaHorasExtras(2000, 500);
     expect(retorno).toBe(esperado);
 }) 
+
+test('retorno dos descontos', ()=> {
+    const esperado = 1500;
+    const retorno = calculaDescontos(2000, 500);
+    expect(retorno).toBe(esperado);
+}) 
