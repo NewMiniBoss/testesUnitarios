adf32dd3bab01b4b3cdecf3a3fe5b94f028d3a53
gabriel <gabriel.f.lazari@gmail.com>
2023-10-05 22:17:02 -0300

    add mais tipos de testes

--- test/pagament.test.js
+++ test/pagament.test.js
@@ -1,13 +1,15 @@
 import { somaHorasExtras, calculaDescontos } from '../index.js';
 
-test('retorno das horas extras', ()=> {
-    const esperado = 2500;
-    const retorno = somaHorasExtras(2000, 500);
-    expect(retorno).toBe(esperado);
-}) 
-
-test('retorno dos descontos', ()=> {
-    const esperado = 1500;
-    const retorno = calculaDescontos(2000, 500);
-    expect(retorno).toBe(esperado);
-}) 
+describe('teste de calculos da folhas', () => {
+    it ('retorno das horas extras', ()=> {
+        const esperado = 2500;
+        const retorno = somaHorasExtras(2000, 500);
+        expect(retorno).toBe(esperado);
+    }) 
+    
+    it ('retorno dos descontos', ()=> {
+        const esperado = 1500;
+        const retorno = calculaDescontos(2000, 500);
+        expect(retorno).toBe(esperado);
+    }) 
+})
